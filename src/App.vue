65e8fbd8378b59e83fea315dcfc7ca7f5b6594be
Olga Draber <olgadraber@gmail.com>
2018-06-27 23:04:48 +0200

    add top navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBarTop />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBarTop from './components/NavBarTop.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBarTop
   }
 }
 </script>
 
 <style>
+body {
+  color: #2c2a30;
+  background-color: #fbfbfb;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NavBarTop.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav-bar-top">
+    <div class="title">{{title}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarTop',
+  data: function() {
+    return {
+      title: "Simple to do application"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.nav-bar-top {
+  border-bottom: 1px #b3a9c6 solid;
+  text-align: center;
+  padding: 20px;
+}
+.nav-bar-top .title {
+  font-weight: 600;
+}
+</style>
